ef3d42b641cf6ea5fa73f44077a538336e44e350
AhmedZeyadTareq <ahmed.adulimi@gmail.com>
2026-05-29 21:58:28 +0300

    Update DDL_Scripts.sql

--- Scripts/Bronze/DDL_Scripts.sql
+++ Scripts/Bronze/DDL_Scripts.sql
@@ -32,21 +32,3 @@ CREATE TABLE bronze.sis_full_ug_enr (
     Mapped_Id NVARCHAR(20),
     Validation_Key NVARCHAR(10),
 )
-
-IF OBJECT_ID('bronze.sis_enr_cleaned', 'U') IS NOT NULL
-    DROP TABLE bronze.sis_enr_cleaned;
-
-CREATE TABLE bronze.sis_enr_cleaned (
-    Term_Code INT,                         -- e.g., 2302
-    Term NVARCHAR(100),                    -- e.g., 2022/2023 Spring-UGRD and ASP
-    Career NVARCHAR(20),                   -- e.g., UGRD
-    College NVARCHAR(100),                 -- e.g., Arts and Sciences
-    Program NVARCHAR(100),                 -- e.g., Biology
-    Id NVARCHAR(20),                       -- preserves leading zero
-    Name NVARCHAR(200),                    -- full name
-    Subject NVARCHAR(20),                  -- e.g., ASP
-    Unit_Taken DECIMAL(5,2),               -- e.g., 0.00
-    Gender CHAR(1),                        -- e.g., F
-    Mapped_Id NVARCHAR(20),                -- e.g., 1231220106
-    Validation_Key NVARCHAR(10),           -- e.g., 33333
-)
